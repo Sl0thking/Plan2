b513a6745a62c6e14b50503ad4fdf0384a18e571
Sl0thking <k.jolitz-dev@outlook.de>
2016-04-28 21:44:22 +0200

    #First Approach
    Erste Seite aufgebaut

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from django.http import HttpResponse
+from django.template import Template, Context
 from foodplan.models import Recipe
 
 def hello(request):
@@ -9,3 +10,10 @@ def hello(request):
     rec.save()
     return HttpResponse("<HTML>Hallo</HTML>")
 
+def showRecipes(request):
+    fp = open('.\\htmlTemplates\\recipeOverview.html')
+    t = Template(fp.read())
+    fp.close()
+    recipes = Recipe.objects.all()
+    html = t.render(Context({'recipes': recipes}))
+    return HttpResponse(html)
